1d7aab8dd9268eea39fe43474b4beb3adf715b0a
bedenchw30 <90423350+bedenchw30@users.noreply.github.com>
2021-09-13 17:13:56 -0400

    initial commit

--- src/main/java/Exerciseeight.java
+++ src/main/java/Exerciseeight.java
@@ -1,4 +1,8 @@
 import java.util.Scanner;
+/**
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 beden chew
+ */
 public class Exerciseeight {
     public static void main (String[] args){
         Scanner input = new Scanner(System.in);
